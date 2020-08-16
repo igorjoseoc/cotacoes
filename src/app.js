f40dbdceca7fa846d05ee9c4948bd4fb36eb0d1a
igorjoseoc <igorjose.oc@gmail.com>
2020-08-16 11:36:29 -0300

    REMOVENDO porta 3001

--- src/app.js
+++ src/app.js
@@ -96,7 +96,7 @@ app.get('*', (req, res) => {
     })
 })
 
-const port = process.env.port || 3001
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log(`server is up on port ${port}`)
